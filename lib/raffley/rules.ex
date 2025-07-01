36e7829dfd1ba3a9cd2e029123062e19a682c75a
Francesc Leveque <francesc@leveque.es>
2025-07-01 08:50:48 +0000

    Chapter 5: Server-Side rendering with HEEx. Rules controller

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,22 @@
+defmodule Raffley.Rules do
+  @moduledoc """
+  This module contains the rules for Raffley.
+  """
+
+  def list_rules do
+    [
+      %{
+        id: 1,
+        text: "Participants must have a high tolerance for puns and dad jokes. 🙃"
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claiming their prize. 💃🕺"
+      },
+      %{
+        id: 3,
+        text: "Have fun! 🎉"
+      }
+    ]
+  end
+end
