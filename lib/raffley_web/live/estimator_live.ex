3cc87cffb27bf0f6db6fb3104dc5d3ecd17fd0ff
Francesc Leveque <francesc@leveque.es>
2025-07-02 22:09:32 +0000

    Chapter 17: Layouts. Modify app layout

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -6,7 +6,7 @@ defmodule RaffleyWeb.EstimatorLive do
       Process.send_after(self(), :tick, 2000)
     end
 
-    socket = assign(socket, tickets: 0, price: 3)
+    socket = assign(socket, tickets: 0, price: 3, page_title: "Raffle Estimator")
 
     # {:ok, socket, layout: {RaffleyWeb.Layouts, :simple}}
     {:ok, socket}
