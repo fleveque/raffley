c1d2a619ea3efab5e26bf336b5258b707ad18572
Francesc Leveque <francesc@leveque.es>
2025-07-02 08:44:44 +0000

    Chapter 13. Listing Raffles. Static LW index page for raffles

---
+++ lib/raffley/raffles.ex
@@ -0,0 +1,42 @@
+defmodule Raffley.Raffle do
+  @moduledoc """
+  This module represents a raffle in the Raffley application.
+  """
+
+  defstruct [:id, :prize, :ticket_price, :status, :image_path, :description]
+end
+
+defmodule Raffley.Raffles do
+  @moduledoc """
+  This module contains the logic for managing raffles.
+  """
+
+  def list_raffles do
+    [
+      %Raffley.Raffle{
+        id: 1,
+        prize: "Autographed Jersey",
+        ticket_price: 2,
+        status: :open,
+        image_path: "/images/jersey.jpg",
+        description: "Step up, sports fans!"
+      },
+      %Raffley.Raffle{
+        id: 2,
+        prize: "Coffee With A Yeti",
+        ticket_price: 3,
+        status: :upcoming,
+        image_path: "/images/yeti-coffee.jpg",
+        description: "A super-chill coffee date."
+      },
+      %Raffley.Raffle{
+        id: 3,
+        prize: "Vintage Comic Book",
+        ticket_price: 1,
+        status: :closed,
+        image_path: "/images/comic-book.jpg",
+        description: "A rare collectible!"
+      }
+    ]
+  end
+end
